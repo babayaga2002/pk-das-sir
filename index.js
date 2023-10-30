6ad0d0b038ce2789141de74eb8aeb9af2ec36766
babayaga2002 <c.sidharth@iitg.ac.in>
2023-10-31 01:01:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  // alert("hi");
+  nav_header.classList.toggle("active");
+};
+
+//  subscribe to thapa technical Channle plz link:https://www.youtube.com/thapatechnical
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
